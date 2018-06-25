1a04c2d3f6e75626b4fb3ef8eefd524354897df6
sdelamo <sergio.delamo@softamo.com>
2018-06-26 01:09:52 +0200

    Changes to guide

--- complete/travis-build.sh
+++ complete/travis-build.sh
@@ -2,10 +2,7 @@
 
 EXIT_STATUS=0
 
-echo "************ Gradle assemble.... *************"
-./gradlew assemble || EXIT_STATUS=$? # // <1>
-
-echo "************ Gradle check.... *************** "
-./gradlew check || EXIT_STATUS=$? # // <2>
+echo "Running tests with geb.env chromeHeadless"
+./gradlew -Dgeb.env=chromeHeadless check || EXIT_STATUS=$? # // <1>
 
 exit $EXIT_STATUS
